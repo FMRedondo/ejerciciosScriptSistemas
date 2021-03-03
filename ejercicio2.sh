bc96395cb93aed1ad98eef0b91f328220e5c519a
FMRedondo <65115094+FMRedondo@users.noreply.github.com>
2021-03-03 18:11:01 +0100

    Delete ejercicio2.sh

--- ejercicio2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "saludos" $1"!"
-
-# sh saludos Carlos -> saludos carlos!
-# sh saludos Lucia Marta -> Saludos Lucia > ya que no coge el segundo parametro $2
-# sh saludos "Lucia Marta" -> Saludos Lucia Marta, ya que coge el string al completo
-# sh saludos (sin argumentos) -> Saludos! Ya que no le hemos pasado un parametro
