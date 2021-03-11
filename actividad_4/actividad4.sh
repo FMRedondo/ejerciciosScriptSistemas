9191eaf879b7b8eccceafbe2110930e82b53561e
FMRedondo <65115094+FMRedondo@users.noreply.github.com>
2021-03-11 17:35:05 +0100

    Add files via upload

--- actividad_4/actividad4.sh
+++ actividad_4/actividad4.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 clear
 
-echo "[ c ] -> Copiara el fichero a un directorio que se pedirá por teclado"
-echo "[ b ] -> Borrara el fichero del directorio donde esta ubicado"
-echo "[ m ] -> moverá el fichero a un directorio que se pedira por teclado"
-echo "[ v ] -> Visualizará el fichero pasdo por parametro"
 
-read menu;
-echo $menu
+if [ $# -eq 1 ];
+then
+    if [ -f $1 ];
+    then
+        echo "[ c ] -> Copiara el fichero a un directorio que se pedirá por teclado"
+        echo "[ b ] -> Borrara el fichero del directorio donde esta ubicado"
+        echo "[ m ] -> moverá el fichero a un directorio que se pedira por teclado"
+        echo "[ v ] -> Visualizará el fichero pasdo por parametro"
+
+        read menu;
+        echo $menu
 
 case $menu in
 c)
@@ -17,8 +22,14 @@ c)
     echo "Elige el directorio donde copiaras el archivo"
     read directorio
 
-    cp $1 $directorio
-    echo "Archivo copiado con exito..."
+    if [ -d $directorio ];
+    then
+        cp $1 $directorio
+        echo "Archivo copiado con exito..."
+    else
+        echo "Achanta que no existe"
+        exit
+    fi;
     ;;
 
 b)
@@ -48,20 +59,29 @@ m)
     echo "¿A que directorio lo quieres mover?"
     read m
 
-    echo "¿Estas seguro que quieres mover el archivo?"
-        echo "[ 1 ] -> si, estoy seguro, muevelo!"
-        echo "[ 2 ] -> No, mejor no, los sitios estan estudiados ;)"
-        read mover
 
-    if [ $mover -eq 1 ];
-        then
-            cp $1 $m
-            echo "Archivo movido con exito"
-        else
-            clear
-            echo "Perfecto, el archivo sigue en su sitio"
+    if [ -d $m ]:
+    then
+        
+        echo "¿Estas seguro que quieres mover el archivo?"
+            echo "[ 1 ] -> si, estoy seguro, muevelo!"
+            echo "[ 2 ] -> No, mejor no, los sitios estan estudiados ;)"
+            read mover
+
+        if [ $mover -eq 1 ];
+            then
+                cp $1 $m
+                echo "Archivo movido con exito"
+            else
+                clear
+                echo "Perfecto, el archivo sigue en su sitio"
+            fi;
+    else
+        echo "El directorio no existe"
         fi;
     ;;
+
+    
 v)
     echo "Has elegido [ Visualizará el fichero pasdo por parametro ]"
     nano $1
@@ -70,3 +90,10 @@ v)
     echo "ERROR > Elige una opción correcta"
 
     esac
+    else
+        sh actividad4.sh
+    fi;
+
+else
+echo "Debes de intoducir un parametro, y este debe de ser un fichero existente"
+fi
